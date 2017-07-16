b1e5b444b93f5318c11141b9be378898c06facad
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-07-17 03:12:44 +0500

    Status entity can be used as communication medium (Payload/Response) in the HttpTask objects.

---
+++ api/src/main/java/sfllhkhan95/android/rest/entity/Status.java
@@ -0,0 +1,29 @@
+package sfllhkhan95.android.rest.entity;
+
+/**
+ * General status messages used to exchange information with the server.
+ *
+ * @author saifkhichi96
+ * @version 1.0
+ */
+public class Status {
+
+    private Integer code;
+    private String message;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
